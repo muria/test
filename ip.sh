0da1d5c431330971afbfd8bf33a687bce5b25714
Martin Uria <martin.uria@gmail.com>
2017-10-19 02:14:07 +0000

    Updated script

--- ip.sh
+++ ip.sh
@@ -5,16 +5,14 @@ ip=$out/ip.txt
 crypt=$out/ip.crypt
 mkdir -p $out
 old=`openssl des3 -d -in $crypt -pass pass:$1`
-echo Old one is $old
 new=`dig +short myip.opendns.com @resolver1.opendns.com`
 echo $new > $ip
-echo New one is $new
 openssl des3 -in $ip -out $crypt -pass pass:$1
 rm $ip
 
 if [ $old != $new ] 
 then
 	git add $crypt
-	git commit -m "Updated crypt"
-	git push
+	git commit -m "Updated crypt" --quiet
+	git push --quiet
 fi
