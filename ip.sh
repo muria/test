aba84062a3eff73abd5d66625cb392084d3e3075
Martin Uria <martin.uria@gmail.com>
2017-10-19 02:06:59 +0000

    Updated script

--- ip.sh
+++ ip.sh
@@ -4,14 +4,17 @@ out=./tmp
 ip=$out/ip.txt
 crypt=$out/ip.crypt
 mkdir -p $out
-dig +short myip.opendns.com @resolver1.opendns.com > $ip
+old=`openssl des3 -d -in $crypt -pass pass:$1`
+echo Old one is $old
+new=`dig +short myip.opendns.com @resolver1.opendns.com`
+echo $new > $ip
+echo New one is $new
 openssl des3 -in $ip -out $crypt -pass pass:$1
 rm $ip
-#decrypt with
-#openssl des3 -d -in ip.crypt -out ip.txt -pass pass:$1
 
-#git status
-#if [ $! -eq 0 ]
-#then
-#echo 'done!'
-#fi
+if [ $old != $new ] 
+then
+	git add $crypt
+	git commit -m "Updated crypt"
+	git push
+fi
